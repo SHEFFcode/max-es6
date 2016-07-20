f4801d2aea8081c2a7daaec5bc17f6665d3419af
Yuriy Shefer <sheff@sheffmachine.com>
2016-07-19 21:56:48 -0700

    fat arrow functions

--- scope.js
+++ scope.js
@@ -8,3 +8,14 @@ const max = 'Maximilian'; // will not allow a change here
 const arr = [25, 33];
 arr.push(35); // this will be allowed
 
+//variable hoisting
+age = 25; // this will error out, lets and consts have to be initialized before being used
+
+let age;
+
+//fat arrow functions
+var fn = () => console.log('hello');
+
+var func = a => a + 5; // if there is only one argument, you can leave off the paranthesis
+
+console.log(func(3));
