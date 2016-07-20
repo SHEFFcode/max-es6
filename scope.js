151e819c1eaf3c8f0196e38855271e5f4d8303ee
Yuriy Shefer <sheff@sheffmachine.com>
2016-07-19 21:49:37 -0700

    consts

--- scope.js
+++ scope.js
@@ -1,6 +1,10 @@
 //let contains the variable within the code block
 if (true) {
-	let name = 'Max';
+	let name = 'Max'; //let is only accessible in this code block
 }
 
-console.log(name);
+//Const does not allow for variable to change
+const max = 'Maximilian'; // will not allow a change here
+const arr = [25, 33];
+arr.push(35); // this will be allowed
+
